c5d93dc7c0c8934bcd6806ed37f5ab2bbdba90b1
jallend1 <jallend1@gmail.com>
2020-04-15 16:53:34 -0700

    Adds laser functionality and laser destruction

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const width = $game.offsetWidth;
 const height = $game.offsetHeight;
 const playerWidth = 20;
 const playerHeight = 30;
-const maxSpeed = 5;
+const maxSpeed = 500;
+const laserSpeed = 5;
+const coolDown = 0.1;
 
 const gameState = {
     playerX: width / 2,
     playerY: height - playerHeight,
     leftPressed: false,
     rightPressed: false,
-    spacePressed: false
+    spacePressed: false,
+    lasers: [],
+    lastTime: Date.now(),
+    coolDown: coolDown
 }
 
 function checkBoundaries(){                                                             // Make sure the ship stays on the field
@@ -18,6 +23,19 @@ function checkBoundaries(){                                                     
         gameState.playerX = playerWidth;
     }else if(gameState.playerX > width - playerWidth){
         gameState.playerX = width - playerWidth;
+    }
+}
+
+function createLasers(x, y, delta){
+    gameState.coolDown -= delta;
+    if(gameState.coolDown < 0){
+        const $newLaser = document.createElement('img');
+        $newLaser.src = 'img/laser-red-1.png';
+        $newLaser.className = 'laser';
+        $game.appendChild($newLaser);
+        setPosition($newLaser, x, y - playerHeight);
+        gameState.lasers.push({x, y, $newLaser});
+        gameState.coolDown = coolDown;
     }
 }
 
@@ -55,14 +73,37 @@ function handleKeyUp(e){
 
 function update(){
     const $player = document.querySelector('.player');
+    const currentTime = Date.now();
+    let delta = (currentTime - gameState.lastTime) / 1000;
     if(gameState.leftPressed === true){
-        gameState.playerX -= maxSpeed;
+        gameState.playerX -= maxSpeed * delta;
     }else if(gameState.rightPressed === true){
-        gameState.playerX += maxSpeed;
+        gameState.playerX += maxSpeed * delta;
+    }
+    if(gameState.spacePressed === true){
+        console.log(delta)
+        createLasers(gameState.playerX, gameState.playerY, delta);
     }
     checkBoundaries();
     setPosition($player, gameState.playerX, gameState.playerY);
+    updateLasers();
+    gameState.lastTime = currentTime;
     requestAnimationFrame(update);
+}
+
+function updateLasers(){
+    let lasersToDelete = 0;
+    gameState.lasers.forEach(laser => {
+        laser.y -= laserSpeed;
+        if(laser.y < 0){
+            $game.removeChild(laser.$newLaser);
+            lasersToDelete++;
+        }
+        else{
+            setPosition(laser.$newLaser, laser.x, laser.y);
+        }
+    })
+    gameState.lasers.splice(0, lasersToDelete);
 }
 
 function init(){
